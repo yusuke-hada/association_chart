8a534f08f93306ac9fa9ef599718bdfa0e933e58
yusuke-hada <aspringwithoutlife4929@gmail.com>
2023-10-22 20:59:13 +0900

    Add:header

--- config/importmap.rb
+++ config/importmap.rb
@@ -3,6 +3,7 @@ pin "application", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
+pin "jquery", to: "jquery/dist/jquery.js"
 pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@5.3.2/dist/js/bootstrap.esm.js"
 pin "@popperjs/core", to: "https://ga.jspm.io/npm:@popperjs/core@2.11.8/lib/index.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
