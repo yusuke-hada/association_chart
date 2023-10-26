96b22f725ac59b66e2e5eef9a849e07e91c284ed
yusuke-hada <aspringwithoutlife4929@gmail.com>
2023-10-26 22:02:44 +0900

    Add:profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
 
   def new
     @user = User.new
@@ -14,11 +15,18 @@ class UsersController < ApplicationController
     end
   end
 
-  def show ;end
+  def show; end
 
   def edit ;end
 
-  def update ;end
+  def update
+    if @user.update(user_params)
+      redirect_to user_path(@user), success: "編集に成功しました"
+    else
+      flash.now[danger] = "編集に失敗しました"
+      render :edit
+    end
+  end
 
   def destroy ;end
 
@@ -28,4 +36,7 @@ class UsersController < ApplicationController
     params.require(:user).permit(:name, :email, :role, :password, :password_confirmation)
   end
 
+  def set_user
+    @user = current_user
+  end
 end
