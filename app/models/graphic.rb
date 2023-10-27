b712a6b95299718630ed23b35a96131f701199f2
yusuke-hada <aspringwithoutlife4929@gmail.com>
2023-10-27 15:58:25 +0900

    Add:graphics_controller

--- app/models/graphic.rb
+++ app/models/graphic.rb
@@ -1,2 +1,3 @@
 class Graphic < ApplicationRecord
+    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
-  resources :users, only: %i(new create show edit update)
   root to: 'top#top'
+  resources :users, only: %i(new create show edit update) do
+    resources :graphics
+  end
 end
